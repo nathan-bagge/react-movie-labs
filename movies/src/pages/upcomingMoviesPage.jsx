07fdb94e0556119d0d6b8bb8f0da961a7b592fd7
Nathan Bagge <nathan@Nathans-MBP.station>
2025-10-18 13:45:37 +0100

    Added Upcoming movies page.

---
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "@tanstack/react-query";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import Spinner from "../components/spinner";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+
+const UpcomingMoviesPage = () => {
+  // Fetch upcoming movies using react-query
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["upcoming"],
+    queryFn: getUpcomingMovies,
+  });
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />;
+      }}
+    />
+  );
+};
+
+export default UpcomingMoviesPage;
